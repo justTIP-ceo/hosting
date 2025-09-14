34487770c52c813e44e00ce26864c9e05e2169f9
Григорий <grigorij@MacBook-Air-Grigorij-2.local>
2025-09-14 23:18:13 +0300

    modified docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://justtip-ceo.github.io/hosting',
+  url: 'https://justtip-ceo.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/hosting/',
